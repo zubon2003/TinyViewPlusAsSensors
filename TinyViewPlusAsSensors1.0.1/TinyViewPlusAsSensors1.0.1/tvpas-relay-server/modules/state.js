43668e6bd5928705536ec110bb02274c18f9d5bc
zubon2003 <zubon2003@gmail.com>
2025-09-09 23:38:19 +0900

    LED
    LED

--- TinyViewPlusAsSensors1.0.1/TinyViewPlusAsSensors1.0.1/tvpas-relay-server/modules/state.js
+++
@@ -1,8 +0,0 @@
-const state = {
-    tvpasReady: false,
-    raceState: { status: 'stopped' },
-    pilotData: {},
-    flickerLength: 100, // Default value, will be updated by heartbeat
-};
-
-module.exports = state;
